10f47fd58c4475e1217e47f63942dda114992204
Snakevil <zsnakevil@gmail.com>
2011-05-21 03:36:54 +0800

    Git branch information PROMPTed now.
    Signed-off-by: Snakevil <zsnakevil@gmail.com>

---
+++ src/profilex/prompt.d/git-branch.sh
@@ -0,0 +1,13 @@
+# ~/.profilex/prompt.d/git-branch.sh
+#
+# AUTHOR     Snakevil Zen <zsnakevil@gmail.com>
+# COPYRIGHT  © 2011 Snakevil.in.
+
+which git > /dev/null && {
+  _PROFILEX_GIT_BRANCH=`git symbolic-ref HEAD 2> /dev/null`
+  [ -z "${_PROFILEX_GIT_BRANCH}" ] && return 0
+  _PROFILEX_GIT_BRANCH=" g${CBlue}*${_PROFILEX_GIT_BRANCH#refs/heads/}"
+}
+
+# vim:ft=sh:fenc=utf-8:ff=unix:tw=75:ts=2:sts=2:et:ai:si
+# vim:nowrap:sw=2:nu:nuw=4:so=5:fen:fdm=marker

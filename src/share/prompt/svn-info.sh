5a28aaa5f8a53d331947d933da69d86997087c8c
Snakevil <zsnakevil@gmail.com>
2011-06-10 19:44:26 +0800

    SVN information are shown in 'prompt.ps1' now.
    Signed-off-by: Snakevil <zsnakevil@gmail.com>

---
+++ src/share/prompt/svn-info.sh
@@ -0,0 +1,14 @@
+# ~/.local/ProfileX/share/prompt/svn-info.sh
+#
+# AUTHOR     Snakevil Zen <zsnakevil@gmail.com>
+# COPYRIGHT  © 2011 Snakevil.in.
+
+'which' svn awk > /dev/null && {
+  _PROFILEX_SVN_INFO=`'svn' info 2> /dev/null \
+    | 'awk' '"Revision:"==$1{print $2}'`
+  [ -z "${_PROFILEX_SVN_INFO}" ] && return 0
+  _PROFILEX_SVN_INFO="${CBlue}(${CBase}s${CBlue}${_PROFILEX_SVN_INFO})"
+}
+
+# vim:ft=sh:fenc=utf-8:ff=unix:tw=75:ts=2:sts=2:et:ai:si
+# vim:nowrap:sw=2:nu:nuw=4:so=5:fen:fdm=marker

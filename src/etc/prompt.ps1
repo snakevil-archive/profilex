bbabca3db9fa46e25b95ce084c6f588549d8c477
Snakevil <zsnakevil@gmail.com>
2011-06-16 21:27:17 +0800

    Alias name of CWD in 'x' be shown in command-lines propmpting.
    Signed-off-by: Snakevil <zsnakevil@gmail.com>

--- src/etc/prompt.ps1
+++ src/etc/prompt.ps1
@@ -1,3 +1,3 @@
 
-%DATE%${CBase}<%USER%${CBase}@%IP%${CBase}:%PWD%%GIT_BRANCH%%HG_BRANCH%%SVN_INFO%${CBase}>
+%DATE%${CBase}<%USER%${CBase}@%IP%${CBase}:%X%%PWD%%GIT_BRANCH%%HG_BRANCH%%SVN_INFO%${CBase}>
 %TIME% ${CBase}[%INDEX%%LOAD%%JOBS%%EXIT%${CBase}] %SYMBOL% %END%
---
+++ src/share/prompt/x.sh
@@ -0,0 +1,11 @@
+# ~/.local/ProfileX/share/prompt/x.sh
+#
+# AUTHOR     Snakevil Zen <zsnakevil@gmail.com>
+# COPYRIGHT  © 2011 Snakevil.in.
+
+_PROFILEX_X=`'xquery' -R 2> /dev/null`
+[ -n "${_PROFILEX_X}" ] && \
+  _PROFILEX_X="${CSilver}${_PROFILEX_X}${CBase}:"
+
+# vim:ft=sh:fenc=utf-8:ff=unix:tw=75:ts=2:sts=2:et:ai:si
+# vim:nowrap:sw=2:nu:nuw=4:so=5:fen:fdm=marker

7ee730e61b0c52c6da54215a349eeab8952dc716
Snakevil <zsnakevil@gmail.com>
2011-06-17 15:23:50 +0800

    PROMPTing variant 'PWD' rewritten for 30~50% performance improved.
    Signed-off-by: Snakevil <zsnakevil@gmail.com>

--- src/share/prompt/pwd.sh
+++ src/share/prompt/pwd.sh
@@ -3,50 +3,53 @@
 # AUTHOR     Snakevil Zen <zsnakevil@gmail.com>
 # COPYRIGHT  © 2011 Snakevil.in.
 
-local homed
-local depth
+[ -n "${_PROFILEX_PWD}" -a "${PWD}" = "${_PROFILEX_pwd}" ] && return
 
-PWD=`pwd`
+local homed
 
 case "${PWD}" in
-  "${HOME}"* )
-    homed=1
-    _PROFILEX_PWD=`'echo' "${PWD}" \
-      | 'sed' -e "s@^${HOME}@@"`
+  "${HOME}" )
+    homed=Y
+    _PROFILEX_PWD=''
+    ;;
+  "${HOME}/"* )
+    homed=Y
+    _PROFILEX_PWD="${PWD:1+${#HOME}}"
     ;;
   * )
-    homed=0
-    _PROFILEX_PWD="${PWD}"
+    homed=
+    _PROFILEX_PWD="${PWD:1}"
     ;;
 esac
 
-'which' awk expr > /dev/null && {
-  depth=`'echo' -n "${_PROFILEX_PWD}" \
-    | 'awk' -F'/' 'END{print NF-1}'`
-  [ 3 -lt $depth ] && {
-    local line
-    local index=0
+[ 0 -lt ${#_PROFILEX_PWD} ] && {
+  'which' awk > /dev/null && {
+    local depth=`'echo' -n "${_PROFILEX_PWD}" \
+      | 'awk' -F'/' 'END{print NF}'`
     _PROFILEX_PWD=`'echo' -n "${_PROFILEX_PWD}" \
-      | 'awk' 'BEGIN{RS="/"}{print}' \
-      | while 'read' line
-        do
-          [ 1 -lt $index -a $index -lt $('expr' $depth - 1) ] && {
-            line="${CUnder}${CYellow}${line:0:1}${COlive}"
-          }
-          [ 0 -eq $index ] || 'echo' -n "/${line}"
-          index=$('expr' $index + 1)
-        done`
-    unset line
-    unset index
+      | 'awk' -v 'CH=${CYellow}${CUnder}' -v 'CN=${COlive}' ' \
+        BEGIN { \
+          RS = "/"; \
+          OFS = ""; \
+          ORS = "/"; \
+        } \
+        2 < NR && '"${depth}"' - 1 > NR { \
+          print CH, substr($0, 0, 1), CN; \
+          next; \
+        } \
+        { \
+          print; \
+        }'`
+    unset depth
+    eval '_PROFILEX_PWD="'"${_PROFILEX_PWD:0:-1}"'"'
   }
+
+  _PROFILEX_PWD="${COlive}/${_PROFILEX_PWD}"
 }
 
-_PROFILEX_PWD="${COlive}${_PROFILEX_PWD}"
 
-[ 1 -eq $homed ] && _PROFILEX_PWD="${CYellow}~${_PROFILEX_PWD}"
+[ -n "${homed}" ] && _PROFILEX_PWD="${CYellow}~${_PROFILEX_PWD}"
 unset homed
-
-unset depth
 
 # vim:ft=sh:fenc=utf-8:ff=unix:tw=75:ts=2:sts=2:et:ai:si
 # vim:nowrap:sw=2:nu:nuw=4:so=5:fen:fdm=marker
